ef6228b600b2060feb0b9ce1953951499c8c12c7
wiori <mikobot731@gmail.com>
2024-06-13 02:06:16 +0800

    Update home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,5 @@
 const { errorHandler } = require("../utils");
 
 exports.home = async (req, res, next) => {
-  res.json({"Authors":"DungUwU && Lê Anh Trí","APIs":"/loli /wallpaper /nino/[get/add]"});
+  res.json({"Authors":"Đây là web APIs của Wioriz","APIs":"/loli /wallpaper /nino/[get/add]"});
 };
